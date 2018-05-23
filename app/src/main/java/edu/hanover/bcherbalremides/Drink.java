e8569a036a57990114a8490d225e8d933d84d192
Hayesb20 <hayesb20@hanover.edu>
2018-05-23 13:47:45 -0400

    updated code

--- app/src/main/java/edu/hanover/bcherbalremides/Drink.java
+++ app/src/main/java/edu/hanover/bcherbalremides/Drink.java
@@ -4,9 +4,9 @@ public class Drink {
     private String name; private String description; private int imageResourceId;
     //drinks is an array of Drinks
     public static final Drink[] drinks = {
-            new Drink("Sencha", "A popular green tea from Japan", R.drawable.Sencha_Green_Tea),
-            new Drink("Wakocha", "A popular black tea from Japan", R.drawable.Sencha_Green_Tea),
-            new Drink("Matcha", "Highest quality beans roasted and brewed fresh", R.drawable.Sencha_Green_Tea)
+            new Drink("Sencha", "A popular green tea from Japan", R.drawable.sencha),
+            new Drink("Wakocha", "A popular black tea from Japan", R.drawable.sencha),
+            new Drink("Matcha", "Highest quality beans roasted and brewed fresh", R.drawable.sencha)
     };
 
     //Each Drink has a name, description, and an image resource
