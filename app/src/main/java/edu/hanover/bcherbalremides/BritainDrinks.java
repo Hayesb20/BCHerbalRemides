db9ce6ad17298837d3064872b1c8734f5acd8290
Patrick Conboy <rispir45@gmail.com>
2018-05-23 14:30:54 -0400

    Added BritishDrinks class

---
+++ app/src/main/java/edu/hanover/bcherbalremides/BritainDrinks.java
@@ -0,0 +1,34 @@
+package edu.hanover.bcherbalremides;
+
+public class BritainDrink {
+    private String name; private String description; private int imageResourceId;
+    //drinks is an array of Drinks
+    public static final BritainDrink[] britainDrinks = {
+            new BritainDrink("Sencha", "A popular green tea from Japan", R.drawable.sencha),
+            new BritainDrink("Wakocha", "A popular black tea from Japan", R.drawable.wakocha),
+            new BritainDrink("Matcha", "Unique powdered green tea only exported from Japan", R.drawable.matcha)
+    };
+
+    //Each Drink has a name, description, and an image resource
+    private Drink(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
