6e1caad5d39f46716bb2dafcad28568dd31ab58f
Hayesb20 <hayesb20@hanover.edu>
2018-05-23 15:28:18 -0400

    changed the layout in activity_drink from lynear to relative

--- app/src/main/java/edu/hanover/bcherbalremides/DrinkActivity.java
+++ app/src/main/java/edu/hanover/bcherbalremides/DrinkActivity.java
@@ -1,9 +1,12 @@
 package edu.hanover.bcherbalremides;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 
 public class DrinkActivity extends Activity {
     public static final String EXTRA_DRINKID = "drinkId";
@@ -30,4 +33,13 @@ public class DrinkActivity extends Activity {
         photo.setImageResource(drink.getImageResourceId());
         photo.setContentDescription(drink.getName());
     }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DelayedMessageService.class);
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE,
+                getResources().getString(R.string.response));
+        startService(intent);
+    }
+
+
 }
