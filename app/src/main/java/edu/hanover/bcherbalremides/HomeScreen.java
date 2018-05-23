8783e50fdb46588eb1f53d652e6978093212427b
Hayesb20 <hayesb20@hanover.edu>
2018-05-23 11:47:09 -0400

    Initial commit

---
+++ app/src/main/java/edu/hanover/bcherbalremides/HomeScreen.java
@@ -0,0 +1,19 @@
+package edu.hanover.bcherbalremides;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.view.View;
+
+public class HomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+    public void onClickJapan
+}
