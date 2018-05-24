8f2ae594d8cbde4266ede8d661feeed3b6995250
Patrick Conboy <rispir45@gmail.com>
2018-05-23 22:38:54 -0400

    Finished setting up BritishTea class

--- app/src/main/java/edu/hanover/bcherbalremides/BritishTea.java
+++ app/src/main/java/edu/hanover/bcherbalremides/BritishTea.java
@@ -1,37 +1,37 @@
-//package edu.hanover.bcherbalremides;
-//
-//
-//public class BritainDrink {
-//    private String name;
-//    private String description;
-//    private int imageResourceId;
-//    //drinks is an array of Drinks
-//    public static final BritainDrink[] britainDrinks = {
-//            new BritainDrink("Sencha", "A popular green tea from Japan", R.drawable.sencha),
-//            new BritainDrink("Wakocha", "A popular black tea from Japan", R.drawable.wakocha),
-//            new BritainDrink("Matcha", "Unique powdered green tea only exported from Japan", R.drawable.matcha)
-//    };
-//
-//    //Each Drink has a name, description, and an image resource
-//    private Drink(String name, String description, int imageResourceId) {
-//        this.name = name;
-//        this.description = description;
-//        this.imageResourceId = imageResourceId;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public int getImageResourceId() {
-//        return imageResourceId;
-//    }
-//
-//    public String toString() {
-//        return this.name;
-//    }
-//}
+package edu.hanover.bcherbalremides;
+
+
+public class BritainDrink {
+    private String name;
+    private String description;
+    private int imageResourceId;
+    //drinks is an array of Drinks
+    public static final BritainDrink[] britainDrinks = {
+            new BritainDrink("Twinings", "The most popular green tea in Britain", R.drawable.twinings),
+            new BritainDrink("PGTips", "A popular black tea from Britain", R.drawable.pgtips),
+            new BritainDrink("Assam", "Unique tea that is drank heavily in Britain", R.drawable.assam)
+    };
+
+    //Each Drink has a name, description, and an image resource
+    private Drink(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
